6da63f4358ea417996abcdbe9903440203ca17af
tiva-OG <tivasalvation@gmail.com>
2025-02-18 07:57:33 +0100

    added basic error handling

--- app.py
+++ app.py
@@ -1,7 +1,32 @@
-from flask import Flask
 import yfinance as yf
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+
+
+class APIError(Exception):
+	"""All custom API Exceptions"""
+	pass
+
+
+class APITickerError(APIError):
+	"""Custom Ticker Error Class"""
+	code = 404
+	description = "Invalid stock ticker or no data available"
+
+
+@app.errorhandler(APIError)
+def handle_exception(err):
+	"""Return custom JSON when APIError or its children are raised """
+	response = {"error": err.description, "message": "Invalid stock ticker or no data available"}
+	return jsonify(response), err.code
+
+
+@app.errorhandler(500)
+def handle_exception(err):
+	"""Return JSON for any other server error"""
+	response = {"error": "Sorry, that error is on us, please contact support if this wasn't an accident", "message": ""}
+	return jsonify(response), 500
 
 
 @app.route("/")
@@ -9,13 +34,13 @@ def index():
 	return "HELLO WORLD"
 
 
-@app.get("/stock/<string:ticker>")
-def get_stock_data(ticker):
+@app.get("/stock/<ticker>")
+def get_stock_data(ticker: str):
 	stock = yf.Ticker(ticker)
 	data = stock.history(period="1d")
 
 	if data.empty:
-		return {"error": "Invalid stock ticker or no data available"}
+		raise APITickerError("Invalid stock ticker or no data available")
 
 	return {
 		"ticker": ticker.upper(),
