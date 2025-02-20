09cc4141c3320dc60bf6365eea45481c1ca45547
tiva-OG <tivasalvation@gmail.com>
2025-02-20 09:05:25 +0100

    created script and database for 'USA50'
    built a script to handle functions and database for arguably the 50 largest companies in the USA

---
+++ usa50.py
@@ -0,0 +1,123 @@
+"""50 Largest Companies in the USA by Market Cap"""
+from datetime import date, datetime
+
+import bs4 as bs
+import requests
+import yfinance as yf
+from sqlalchemy import Integer, Date, Float,  String, create_engine
+from sqlalchemy.orm import Mapped, declarative_base, mapped_column, sessionmaker
+
+Base = declarative_base()
+
+
+class Stock(Base):
+	__tablename__ = "usa50"
+
+	id: Mapped[int] = mapped_column(primary_key=True, index=True)
+	ticker: Mapped[str] = mapped_column(Integer, unique=True)
+	fullname: Mapped[str] = mapped_column(String)
+	sector: Mapped[str] = mapped_column(String)
+	industry: Mapped[str] = mapped_column(String)
+
+	def __repr__(self):
+		return f"Stock(ticker={self.ticker!r},  fullname={self.fullname!r}, sector={self.sector!r}, industry={self.industry!r})"
+
+
+class StockData(Base):
+	__tablename__ = "stock_data"
+
+	id: Mapped[int] = mapped_column(primary_key=True, index=True)
+	ticker: Mapped[str] = mapped_column(String)
+	date: Mapped[datetime] = mapped_column(Date)
+	open: Mapped[float] = mapped_column(Float)
+	close: Mapped[float] = mapped_column(Float)
+	high: Mapped[float] = mapped_column(Float)
+	low: Mapped[float] = mapped_column(Float)
+	volume: Mapped[int] = mapped_column(Integer)
+
+	def __repr__(self):
+		return f"StockData(ticker={self.ticker!r},  date={self.date!r}, open={self.open!r}, close={self.close!r}, high={self.high!r}), low={self.low!r}, volume={self.volume}"
+
+
+engine_usa50 = create_engine("sqlite:///usa50.db")
+engine_stock_data = create_engine("sqlite:///stock_data.db")
+Session = sessionmaker()
+Session.configure(binds={Stock: engine_usa50, StockData: engine_stock_data})
+
+
+def scrape_usa50_tickers() -> list:
+	url = "https://companiesmarketcap.com/usa/largest-companies-in-the-usa-by-market-cap/"
+
+	try:
+		response = requests.get(url)
+		soup = bs.BeautifulSoup(response.text, "lxml")
+		stocks = [div.text for div in soup.find_all("div", {"class": "company-code"})[:50]]
+		return stocks
+	except Exception as e:
+		print(e)
+		return []
+
+
+def get_stock_history(ticker: str) -> list:
+	stock = yf.Ticker(ticker)
+	stock_data = stock.history(period="3y")
+
+	return [
+		{
+			"date": row_id.date(),
+			"open": round(row["Open"], 2),
+			"close": round(row["Close"], 2),
+			"high": round(row["High"], 2),
+			"low": round(row["Low"], 2),
+			"volume": int(row["Volume"])
+		}
+		for row_id, row in stock_data.iterrows()]
+
+
+def get_stock_info(ticker: str) -> dict:
+	stock = yf.Ticker(ticker)
+	stock_info = stock.info
+
+	return {
+		"fullname": stock_info["longName"],
+		"sector": stock_info["sector"],
+		"industry": stock_info["industry"]
+	}
+
+
+def query_all_stocks():
+	with Session() as session:
+		results = session.query(Stock).order_by(Stock.ticker).all()
+		return results
+
+
+def query_stock_history(ticker: str):
+	with Session() as session:
+		results = session.query(StockData).filter(StockData.ticker == ticker).order_by(StockData.date.desc()).all()
+		return results
+
+
+def main() -> None:
+	usa50_tickers = scrape_usa50_tickers()
+	with Session() as session:
+		for ticker in usa50_tickers:
+			print(f"processing {ticker}")
+
+			stock_info = get_stock_info(ticker)
+			stock = Stock(ticker=ticker, fullname=stock_info.get("fullname"), sector=stock_info.get("sector"), industry=stock_info.get("industry"))
+			session.add(stock)
+
+			stock_data = get_stock_history(ticker)
+			for data in stock_data:
+				_data = StockData(ticker=ticker, date=data.get("date"), open=data.get("open"), close=data.get("close"), high=data.get("high"), low=data.get("low"), volume=data.get("volume"))
+				session.add(_data)
+			print(_data)
+
+		session.commit()
+	print("Filling database complete!")
+
+
+if __name__ == "__main__":
+	Base.metadata.tables["usa50"].create(bind=engine_usa50)
+	Base.metadata.tables["stock_data"].create(bind=engine_stock_data)
+	main()
